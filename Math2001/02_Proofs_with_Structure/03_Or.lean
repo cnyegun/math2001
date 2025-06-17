f2995c288fe4af769fa49a12bc2bd02de0c61838
cnyegun <buichinguyen1105@gmail.com>
2025-06-17 15:37:54 +0700

    2.3 exercises

--- Math2001/02_Proofs_with_Structure/03_Or.lean
+++ Math2001/02_Proofs_with_Structure/03_Or.lean
@@ -11,7 +11,7 @@ example {x y : ℝ} (h : x = 1 ∨ y = -1) : x * y + x = y + 1 := by
     x * y + x = 1 * y + 1 := by rw [hx]
     _ = y + 1 := by ring
   calc
-    x * y + x = x * -1 + x := by rw [hy]
+    x * y + x = x * - 1 + x := by rw [hy]
     _ = -1 + 1 := by ring
     _ = y + 1 := by rw [hy]
 
@@ -22,14 +22,19 @@ example {n : ℕ} : n ^ 2 ≠ 2 := by
   calc
     n ^ 2 ≤ 1 ^ 2 := by rel [hn]
     _ < 2 := by numbers
-  sorry
+
+  apply ne_of_gt
+  calc
+    n ^ 2 >= 2 ^ 2 := by rel [hn]
+    _ = 4 := by ring
+    _ > 2 := by numbers
 
 example {x : ℝ} (hx : 2 * x + 1 = 5) : x = 1 ∨ x = 2 := by
   right
   calc
-    x = (2 * x + 1 - 1) / 2 := by ring
-    _ = (5 - 1) / 2 := by rw [hx]
-    _ = 2 := by numbers
+    x = (2 * x + 1) / 2 - 1 / 2 := by ring
+    _ = 5 / 2 - 1 / 2 := by rw [hx]
+    _ = 2 := by ring
 
 
 example {x : ℝ} (hx : x ^ 2 - 3 * x + 2 = 0) : x = 1 ∨ x = 2 := by
@@ -38,7 +43,16 @@ example {x : ℝ} (hx : x ^ 2 - 3 * x + 2 = 0) : x = 1 ∨ x = 2 := by
     (x - 1) * (x - 2) = x ^ 2 - 3 * x + 2 := by ring
     _ = 0 := by rw [hx]
   have h2 := eq_zero_or_eq_zero_of_mul_eq_zero h1
-  sorry
+  obtain h3 | h4 := h2
+  left
+  calc x = (x - 1) + 1 := by ring
+       _ = 0 + 1 := by rw [h3]
+       _ = 1 := by ring
+
+  right
+  calc x = x - 2 + 2 := by ring
+       _ = 0 + 2 := by rw [h4]
+       _ = 2 := by ring
 
 example {n : ℤ} : n ^ 2 ≠ 2 := by
   have hn0 := le_or_succ_le n 0
@@ -72,43 +86,162 @@ example {n : ℤ} : n ^ 2 ≠ 2 := by
 
 
 example {x : ℚ} (h : x = 4 ∨ x = -4) : x ^ 2 + 1 = 17 := by
-  sorry
+  obtain h1 | h2 := h
+  calc
+    x ^ 2 + 1 = 4 ^ 2 + 1 := by rw [h1]
+    _ = 17 := by ring
+  calc
+    x ^ 2 +1 = (-4) ^ 2 + 1 := by rw [h2]
+    _ = 17 := by ring
 
 example {x : ℝ} (h : x = 1 ∨ x = 2) : x ^ 2 - 3 * x + 2 = 0 := by
-  sorry
+  obtain h1 | h2 := h
+  calc
+    x ^ 2 - 3 * x + 2 = 1 ^ 2 - 3 * 1 + 2 := by rw [h1]
+    _ = 0 := by ring
+  calc
+    x ^ 2 - 3 * x + 2 = 2 ^ 2 - 3 * 2 + 2 := by rw [h2]
+    _ = 0 := by ring
 
 example {t : ℚ} (h : t = -2 ∨ t = 3) : t ^ 2 - t - 6 = 0 := by
-  sorry
+  obtain h | h := h
+  calc
+    t ^ 2 - t - 6 = (-2) ^ 2 - (-2) - 6 := by rw [h]
+    _ = 0 := by ring
+
+  calc
+    t ^ 2 - t - 6 = (3) ^ 2 - (3) - 6 := by rw [h]
+    _ = 0 := by ring
 
 example {x y : ℝ} (h : x = 2 ∨ y = -2) : x * y + 2 * x = 2 * y + 4 := by
-  sorry
+  obtain h | h := h
+  calc
+    x * y + 2 * x = 2 * y + 2 * 2 := by rw [h]
+    _ = 2 * y + 4 := by ring
+  calc
+    x * y + 2 * x = x * -2 + 2 * x := by rw [h]
+    _ = 2 * - 2 + 4 := by ring
+    _ = 2 * y + 4 := by rw [h]
 
 example {s t : ℚ} (h : s = 3 - t) : s + t = 3 ∨ s + t = 5 := by
-  sorry
+  left
+  calc
+    s + t = 3 - t + t := by rw [h]
+    _ = 3 := by ring
 
 example {a b : ℚ} (h : a + 2 * b < 0) : b < a / 2 ∨ b < - a / 2 := by
-  sorry
+  right
+  addarith [h]
 
 example {x y : ℝ} (h : y = 2 * x + 1) : x < y / 2 ∨ x > y / 2 := by
-  sorry
+  left
+  calc
+    y / 2 = (2 * x + 1) / 2 := by rw [h]
+    _ = (2 * x) / 2 + 1 / 2 := by ring
+    _ > (2 * x) / 2 := by extra
+    _ = x := by ring
 
 example {x : ℝ} (hx : x ^ 2 + 2 * x - 3 = 0) : x = -3 ∨ x = 1 := by
-  sorry
+  have h1 :=
+    calc
+      (x + 3) * (x - 1) = x ^ 2 + 2 * x - 3 := by ring
+      _ = 0 := by rw [hx]
+  have h2 := eq_zero_or_eq_zero_of_mul_eq_zero h1
+  obtain h | h := h2
+  left
+  calc
+    x = x + 3 - 3 := by ring
+    _ = 0 - 3 := by rw [h]
+    _ = -3 := by ring
+  right
+  calc
+    x = x - 1 + 1 := by ring
+    _ = 0 + 1 := by rw [h]
+    _ = 1 := by ring
 
 example {a b : ℝ} (hab : a ^ 2 + 2 * b ^ 2 = 3 * a * b) : a = b ∨ a = 2 * b := by
-  sorry
+  have h1 :=
+  calc
+    (a - b) * (a - 2 * b) = a ^ 2 + 2 * b ^ 2 - 3 * a * b := by ring
+    _ = 3 * a * b - 3 * a * b := by rw [hab]
+    _ = 0 := by ring
+  have h2 := eq_zero_or_eq_zero_of_mul_eq_zero h1
+  obtain h | h := h2
+  left
+  addarith [h]
+  right
+  addarith [h]
 
 example {t : ℝ} (ht : t ^ 3 = t ^ 2) : t = 1 ∨ t = 0 := by
-  sorry
+  have h : t ^ 2 * (t - 1) = 0 :=
+  calc
+    t ^ 2 * (t - 1) = t ^ 3 - t ^ 2 := by ring
+    _ = t ^ 2 - t ^ 2 := by rw [ht]
+    _ = 0 := by ring
+  have h2 := eq_zero_or_eq_zero_of_mul_eq_zero h
+  obtain h | h := h2
+  right
+  cancel 2 at h
+  left
+  addarith [h]
 
 example {n : ℕ} : n ^ 2 ≠ 7 := by
-  sorry
+  have hn := le_or_succ_le n 2
+  obtain h | h := hn
+  apply ne_of_lt
+  calc
+    n ^ 2 = n * n := by ring
+    _ <= 2 * 2 := by rel [h]
+    _ = 4 := by ring
+    _ < 7 := by numbers
+  apply ne_of_gt
+  calc
+    n ^ 2 = n * n := by ring
+    _ >= 3 * 3 := by rel [h]
+    _ = 9 := by ring
+    _ > 7 := by numbers
 
 example {x : ℤ} : 2 * x ≠ 3 := by
-  sorry
+  have hx := le_or_succ_le x 1
+  obtain h | h := hx
+  apply ne_of_lt
+  calc
+    2 * x = x + x := by ring
+    _ <= 1 + 1 := by rel [h]
+    _ = 2 := by ring
+    _ < 3 := by numbers
+  apply ne_of_gt
+  calc
+    2 * x = x + x := by ring
+    _ >= 2 + 2 := by rel [h]
+    _ = 4 := by ring
+    _ > 3 := by numbers
+
 
 example {t : ℤ} : 5 * t ≠ 18 := by
-  sorry
+  have ht := le_or_succ_le t 3
+  obtain h | h := ht
+  apply ne_of_lt
+  calc
+    5 * t <= 5 * 3 := by rel [h]
+    _ = 15 := by ring
+    _ < 18 := by numbers
+  apply ne_of_gt
+  calc
+    5 * t >= 5 * 4 := by rel [h]
+    _ = 20 := by ring
+    _ > 18 := by numbers
 
 example {m : ℕ} : m ^ 2 + 4 * m ≠ 46 := by
-  sorry
+  have hm := le_or_succ_le m 5
+  obtain h | h := hm
+  apply ne_of_lt
+  calc
+    m ^ 2 + 4 * m <= 5 ^ 2 + 4 * 5 := by rel [h]
+    _ = 45 := by ring
+    _ < 46 := by numbers
+  apply ne_of_gt
+  calc
+    m ^ 2 + 4 * m >= 6 ^ 2 + 4 * 6 := by rel [h]
+    _ = 60 := by ring
+    _ > 46 := by numbers
