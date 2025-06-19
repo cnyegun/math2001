7eb34135623f68de9152aeaae2414f09986acac0
cnyegun <buichinguyen1105@gmail.com>
2025-06-19 18:24:54 +0700

    2.5 exercise --TODO last question

--- Math2001/02_Proofs_with_Structure/05_Exists.lean
+++ Math2001/02_Proofs_with_Structure/05_Exists.lean
@@ -97,13 +97,66 @@ example (x : ℚ) : ∃ y : ℚ, y ^ 2 > x := by
     _ > x := by extra
 
 example {t : ℝ} (h : ∃ a : ℝ, a * t + 1 < a + t) : t ≠ 1 := by
-  sorry
+  obtain ⟨x, hxt⟩ := h
+  have h1 : (1 - x) * (t - 1) > 0 := by
+    calc
+      (1 - x) * (t - 1) = x + t - (x * t + 1) := by ring
+      _ > (x * t + 1) - (x * t + 1) := by rel [hxt]
+      _ = 0 := by ring
+  have h2 := le_or_gt x 1
+  obtain hx_le_1 | hx_gt_1 := h2
+  . have h3 : 1 - x >= 0 := by addarith [hx_le_1]
+    cancel (1 - x) at h1
+    have h4 : t > 1 := by addarith [h1]
+    apply ne_of_gt
+    apply h4
+  . have h3 : (x - 1) * (1 - t) > 0 := by
+      calc
+        (x - 1) * (1 - t) = x + t - (x * t + 1) := by ring
+        _ > (x * t + 1) - (x * t + 1) := by rel [hxt]
+        _ = 0 := by ring
+    have h4 : x - 1 > 0 := by addarith [hx_gt_1]
+    cancel (x - 1) at h3
+    have h5 : t < 1 := by addarith [h3]
+    apply ne_of_lt
+    apply h5
+
 
 example {m : ℤ} (h : ∃ a, 2 * a = m) : m ≠ 5 := by
-  sorry
+  obtain ⟨a, ham⟩ := h
+  have H := le_or_succ_le a 2
+  obtain h_le_2 | h_ge_3 := H
+  apply ne_of_lt
+  calc
+    m = 2 * a := by rw [ham]
+    _ <= 2 * 2 := by rel [h_le_2]
+    _ = 4 := by ring
+    _ < 5 := by numbers
+  apply ne_of_gt
+  calc
+    m = 2 * a := by rw [ham]
+    _ >= 2 * 3 := by rel [h_ge_3]
+    _ = 6 := by ring
+    _ > 5 := by numbers
 
 example {n : ℤ} : ∃ a, 2 * a ^ 3 ≥ n * a + 7 := by
-  sorry
+  obtain h_le_0 | h_gt_1 := le_or_succ_le n 0
+  use 3
+  calc
+    n * 3 + 7 <= 0 * 3 + 7 := by rel [h_le_0]
+    _ = 7 := by ring
+    _ <= 54 := by numbers
+    _ = 2 * 3 ^ 3 := by ring
+  use (n + 1)
+  calc
+    2 * (n + 1) ^ 3 = 2 * n ^ 3 + 6 * n ^ 2 + 6 * n + 2 := by ring
+    _ = 2 * n ^ 3 + 5 * n ^ 2 + 5 * n + n ^ 2 + n + 2 := by ring
+    _ >= 2 * 1 ^ 3 + 5 * 1 ^ 2 + 5 * 1 + n ^ 2 + n + 2 := by rel [h_gt_1]
+    _ = n ^ 2 + n + 7 + 7 := by ring
+    _ >= n ^ 2 + n + 7 := by extra
+    _ = n * (n + 1) + 7 := by ring
+
+
 
 example {a b c : ℝ} (ha : a ≤ b + c) (hb : b ≤ a + c) (hc : c ≤ a + b) :
     ∃ x y z, x ≥ 0 ∧ y ≥ 0 ∧ z ≥ 0 ∧ a = y + z ∧ b = x + z ∧ c = x + y := by
